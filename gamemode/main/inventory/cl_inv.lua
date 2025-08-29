4897e36824da773355f8163f9d2dc1e60fc60834
Nyaaa <5182757+oNyaaa1@users.noreply.github.com>
2025-08-29 15:45:43 +0100

    update

--- gamemode/main/inventory/cl_inv.lua
+++ gamemode/main/inventory/cl_inv.lua
@@ -4,9 +4,16 @@ local frame = nil
 function Middle()
     if frame then return end
     frame = vgui.Create("DPanel")
-    frame:SetSize(800, 418)
-    frame:Center()
-    frame:MakePopup()
+    frame:SetSize(530, 418)
+    frame:SetPos(w * 0.33, h * 0.38)
+    frame.Paint = function(me)
+            surface.SetMaterial(Material("materials/ui/background.png"))
+            surface.SetDrawColor(0, 0, 0, 255)
+            surface.DrawTexturedRect(0, 0, w, h)
+            surface.SetDrawColor(94, 94, 94, 150)
+            surface.DrawRect(0, 0, w, h)
+            DrawMotionBlur(0.5,0.2,0)
+        end
     local grid = vgui.Create("ThreeGrid", frame)
     grid:Dock(FILL)
     grid:DockMargin(4, 4, 4, 4)
@@ -30,9 +37,7 @@ function Middle()
 end
 
 function GM:ScoreboardShow()
-    if not IsValid(frame) then
-        Middle()
-    end
+    if not IsValid(frame) then Middle() end
 end
 
 function GM:ScoreboardHide()
@@ -41,6 +46,7 @@ function GM:ScoreboardHide()
         frame = nil
     end
 end
+
 local function FixedWidth(ww, ex)
     ex = ex or 0
     return w / 2 * ww + ex
@@ -55,22 +61,22 @@ local DPanel = {}
 function Bottom()
     local Spacing = 10
     DPanel[1] = vgui.Create("DPanel")
-    DPanel[1]:SetPos(FixedWidth(0.7), h * 0.85)
+    DPanel[1]:SetPos(FixedWidth(0.65, 20), h * 0.85)
     DPanel[1]:SetSize(80, 80)
     DPanel[2] = vgui.Create("DPanel")
-    DPanel[2]:SetPos(FixedWidth(0.81), h * 0.85)
+    DPanel[2]:SetPos(FixedWidth(0.73, 40), h * 0.85)
     DPanel[2]:SetSize(80, 80)
     DPanel[3] = vgui.Create("DPanel")
-    DPanel[3]:SetPos(FixedWidth(0.82, 80), h * 0.85)
+    DPanel[3]:SetPos(FixedWidth(0.73, 125), h * 0.85)
     DPanel[3]:SetSize(80, 80)
     DPanel[4] = vgui.Create("DPanel")
-    DPanel[4]:SetPos(FixedWidth(0.82, 80 * 2 + Spacing), h * 0.85)
+    DPanel[4]:SetPos(FixedWidth(0.73, 210), h * 0.85)
     DPanel[4]:SetSize(80, 80)
     DPanel[5] = vgui.Create("DPanel")
-    DPanel[5]:SetPos(FixedWidth(0.82, 80 * 3 + Spacing + 10), h * 0.85)
+    DPanel[5]:SetPos(FixedWidth(0.73, 295), h * 0.85)
     DPanel[5]:SetSize(80, 80)
     DPanel[6] = vgui.Create("DPanel")
-    DPanel[6]:SetPos(FixedWidth(0.82, 80 * 4 + Spacing + 20), h * 0.85)
+    DPanel[6]:SetPos(FixedWidth(0.73, 380), h * 0.85)
     DPanel[6]:SetSize(80, 80)
     for i = 1, 6 do
         DPanel[i].Paint = function(me)
